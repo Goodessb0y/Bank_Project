eaf44ff0fbac0755ec754a365de4c6de6085d4a2
Sukharev_Alexandr <suxarev.a86@gmail.com>
2026-03-22 12:28:46 +0300

    update widget func

--- bank_widget/main.py
+++ bank_widget/main.py
@@ -1,12 +1,12 @@
 from bank_widget.src.masks import get_mask_card_number, get_mask_account
 from bank_widget.src.widget import mask_account_card, get_date
 
-# data_time = input()
-card_or_bill = input()
+data_time = input()
+# card_or_bill = input()
 # card = input()
 # account = input()
 #
 # print(get_mask_card_number(card))
 # print(get_mask_account(account))
-print(mask_account_card(card_or_bill))
-# print(get_date(data_time))
+# print(mask_account_card(card_or_bill))
+print(get_date(data_time))
--- bank_widget/src/widget.py
+++ bank_widget/src/widget.py
@@ -1,26 +1,19 @@
-import  re
-
 from bank_widget.src.masks import get_mask_account, get_mask_card_number
-
-
-def is_cyrrilic(bank_data):
-    """Проверка на вхождение кириллицы в строку"""
-    return bool(re.search("[а-яА-Я]", bank_data))
-
+from datetime import datetime
 
 def mask_account_card(card_or_bill: str) -> str:
-    """Маскировка данных (номер карты или счета)"""
-    mask_data = ''
-    if is_cyrrilic(card_or_bill):
-        mask_data = get_mask_account(card_or_bill[5:])
-        return f'Счет {mask_data}'
+    """Маскировка данных (номер счета или карты)"""
+    parts = card_or_bill.rsplit(' ', 1)
+
+    name, number = parts
+
+    if name.lower().startswith('счет'):
+        return f'{name} {get_mask_account(number)}'
     else:
-        mask_data = get_mask_card_number(card_or_bill[-16:])
-        return f'{card_or_bill[:-16]}{mask_data}'
+        return f'{name} {get_mask_card_number(number)}'
 
 
 def get_date(data_time: str) -> str:
     """Смена формата даты"""
-    time_list = data_time[:10].split('-')
-    correct_time = '.'.join(time_list[::-1])
-    return correct_time
+    dt = datetime.fromisoformat(data_time)
+    return dt.strftime('%d.%m.%Y')
