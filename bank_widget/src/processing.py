d38547a8d80fb8a451c91b61f31fe192e75d48fb
Sukharev_Alexandr <suxarev.a86@gmail.com>
2026-03-26 21:21:03 +0300

    add README and correct type annotations/docstrings

--- bank_widget/src/processing.py
+++ bank_widget/src/processing.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+from typing import Any
 
-
-def filter_by_state(data: list, state: str = "EXECUTED") -> list:
+def filter_by_state(data: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """
-    Функция принимает список словарей (данные банковских операций)
-    Возвращает отфильтрованный по параметру state список словарей
+    args:
+        data: список словарей с операциями
+        state: статус операции
+    return:
+        отфильтрованный список операций
     """
 
     filtered = []
@@ -14,11 +17,13 @@ def filter_by_state(data: list, state: str = "EXECUTED") -> list:
 
     return filtered
 
-
-def sort_by_date(data: list, sort=True) -> list:
+def sort_by_date(data: list[dict[str, Any]], sort: bool = True) -> list[dict[str, Any]]:
     """
-    Функция принимает список словарей и параметр порядка сортировки
-    Возвращает отсортированный список согласно параметру sort
+    args:
+        data: список словарей с операциями
+        sort: параметр сортировки
+    return:
+        отсортированный по дате список операций
     """
     sorted_list = sorted(data, key=lambda item: datetime.fromisoformat(item["date"]), reverse=sort)
 
