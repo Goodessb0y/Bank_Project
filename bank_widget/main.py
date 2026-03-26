9b2bafac2f83ad6480000e08f4eaf1a71f4c72bd
Sukharev_Alexandr <suxarev.a86@gmail.com>
2026-03-26 16:34:25 +0300

    update main.py

--- bank_widget/main.py
+++ bank_widget/main.py
@@ -2,7 +2,7 @@ from src.widget import get_date, mask_account_card
 from src.masks import get_mask_account, get_mask_card_number
 
 
-data_time = input()
+# data_time = input()
 # card_or_bill = input()
 # card = input()
 # account = input()
@@ -10,4 +10,4 @@ data_time = input()
 # print(get_mask_card_number(card))
 # print(get_mask_account(account))
 # print(mask_account_card(card_or_bill))
-print(get_date(data_time))
+# print(get_date(data_time))
