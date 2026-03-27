3baff5bc1db2fa3cd85d7934730d466f02a80b2e
Goodessb0y <melinikd6.ss@gmail.com>
2026-03-27 22:02:56 +0300

    Revert "Feature/homework 10 1"

--- bank_widget/main.py
+++ bank_widget/main.py
@@ -1,6 +1,5 @@
 from src.widget import get_date, mask_account_card
 from src.masks import get_mask_account, get_mask_card_number
-from src.processing import filter_by_state, sort_by_date
 
 
 # data_time = input()
@@ -12,4 +11,3 @@ from src.processing import filter_by_state, sort_by_date
 # print(get_mask_account(account))
 # print(mask_account_card(card_or_bill))
 # print(get_date(data_time))
-# print(filter_by_state(list_data)) 
