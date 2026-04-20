b5a139c73f5d4bbf9e1d7dbc99f68c8dba742db8
Sukharev_Alexandr <suxarev.a86@gmail.com>
2026-04-20 11:26:21 +0300

    all done

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ## Описание
 Проект содержит функции для обработки банковских операций:
-- фильтрация операций по статусу
+- фильтрация операций по статусу, валюте
 - сортировка операций по дате
 - маскировка номера карт
 - маскировка личного счета
+- получение описания каждой транзакции
+- получение нового номера карты
 
 ## Установка
 
@@ -16,7 +18,8 @@ git clone https://github.com/Goodessb0y/Bank_Project.git
 Перейти в папку проекта
 
 
-## Пример использования функций фильтрации и сортировки
+## Пример использования функций:
+### Фильтрация по статусу и сортировка по дате
 
 ```python
 from src.processing import filter_by_state, sort_by_date
@@ -24,6 +27,33 @@ from src.processing import filter_by_state, sort_by_date
 data = [...]
 filtered = filter_by_state(data)
 sorted_data = sort_by_date(filtered)
+```
+
+### Фильтрация транзакций по валютному коду
+```python
+from src.generators import filter_by_currency
+
+data = [...]
+code = 'USD'
+
+filter_by_currency(data, code)
+```
+
+### Получение описания транзакции
+```python
+from src.generators import transaction_descriptions
+
+data = [...]
+transaction_descriptions(data)
+```
+
+### Получение нового номера карты
+```python
+from src.generators import card_number_generator
+
+start = 0
+end = 10000
+card_number_generator(start, end)
 ```
 
 ## Тестирование
--- src/generators.py
+++ src/generators.py
@@ -4,13 +4,12 @@ from typing import Any, Iterator
 def filter_by_currency(transactions: list[dict[str, Any]], code: str) -> Iterator[dict]:
     """Генератор yielding транзакции отфильтрованные по коду.
 
-Args:
-    transactions: список с словарем транзакций
-    code: валютная аббревиатура (e.g. 'USD')
+    Args:
+        transactions: список с словарем транзакций
+        code: валютная аббревиатура (e.g. 'USD')
 
-Yields:
-    dict: транзакция, соответствующая указанной валюте
-"""
+    Yields:
+        dict: транзакция, соответствующая указанной валюте"""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["code"] == code:
             yield transaction
@@ -19,12 +18,11 @@ Yields:
 def transaction_descriptions(transaction_data: list[dict[str, Any]]) -> Iterator[dict]:
     """Генератор yielding описание транзакций.
 
-Args:
-    transaction_data: список с словарем транзакций
+    Args:
+        transaction_data: список с словарем транзакций
 
-Yields:
-    str: описание транзакции
-"""
+    Yields:
+        str: описание транзакции"""
     for transaction in transaction_data:
         yield transaction["description"]
 
@@ -32,14 +30,13 @@ Yields:
 def card_number_generator(start: int, end: int):
     """Генератор yielding отформатированный номер карты в заданном диапазоне.
 
-Args:
-    start: начало диапазона
-    end: конец диапазона
+    Args:
+        start: начало диапазона
+        end: конец диапазона
 
-Yields:
-    str: отформатированный номер карты
-"""
+    Yields:
+        str: отформатированный номер карты"""
     for number in range(start, end + 1):
         s = str(number).zfill(16)
-        parts = [s[i:i+4] for i in range(0, 16, 4)]
+        parts = [s[i : i + 4] for i in range(0, 16, 4)]
         yield " ".join(parts)
